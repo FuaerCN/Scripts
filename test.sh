8c90948416c32c09181752848b2ba926f33350ba
Yeasty <fuaercn@gmail.com>
2018-08-09 03:22:34 +0800

    no message

--- test.sh
+++ test.sh
@@ -5,9 +5,9 @@ apt-get install -y ca-certificates
 caddyfile="/usr/local/caddy/"
 caddy_file="/usr/local/caddy/caddy"
 caddy_conf_file="/usr/local/caddy/Caddyfile"
-aria2ng_new_ver=$(wget -qO- https://api.github.com/repos/mayswind/AriaNg/releases/latest | grep -o '"tag_name": ".*"' | sed 's/"//g' | sed 's/tag_name: //g') && echo ${Ver}
+aria2ng_new_ver=$(wget --no-check-certificate -qO- https://api.github.com/repos/mayswind/AriaNg/releases/latest | grep -o '"tag_name": ".*"' | sed 's/"//g' | sed 's/tag_name: //g') && echo ${Ver}
 aria2ng_download_http="https://github.com/mayswind/AriaNg/releases/download/${aria2ng_new_ver}/aria-ng-${aria2ng_new_ver}.zip"
-aria2_new_ver=$(wget -qO- "https://github.com/q3aql/aria2-static-builds/tags"| grep "/q3aql/aria2-static-builds/releases/tag/"| head -n 1| awk -F "/tag/v" '{print $2}'| sed 's/\">//') && echo -e "${aria2_new_ver}"
+aria2_new_ver=$(wget --no-check-certificate -qO- "https://github.com/q3aql/aria2-static-builds/tags"| grep "/q3aql/aria2-static-builds/releases/tag/"| head -n 1| awk -F "/tag/v" '{print $2}'| sed 's/\">//') && echo -e "${aria2_new_ver}"
 
 Install_php(){
 	#Debian 8系统
@@ -15,7 +15,7 @@ Install_php(){
 	echo "deb http://packages.dotdeb.org jessie all" | tee --append /etc/apt/sources.list
 	echo "deb-src http://packages.dotdeb.org jessie all" | tee --append /etc/apt/sources.list
 	#添加key
-	wget https://www.dotdeb.org/dotdeb.gpg
+	wget --no-check-certificate https://www.dotdeb.org/dotdeb.gpg
 	apt-key add dotdeb.gpg
 	#更新系统
 	apt-get update -y
@@ -40,11 +40,11 @@ Install_caddy(){
 	[[ -e "caddy_linux*.tar.gz" ]] && rm -rf "caddy_linux*.tar.gz"
 	
 	if [[ ${bit} == "i386" ]]; then
-		wget -O "caddy_linux.tar.gz" "https://caddyserver.com/download/linux/386?license=personal" && caddy_bit="caddy_linux_386"
+		wget --no-check-certificate -O "caddy_linux.tar.gz" "https://caddyserver.com/download/linux/386?license=personal" && caddy_bit="caddy_linux_386"
 	elif [[ ${bit} == "i686" ]]; then
-		wget -O "caddy_linux.tar.gz" "https://caddyserver.com/download/linux/386?license=personal" && caddy_bit="caddy_linux_386"
+		wget --no-check-certificate -O "caddy_linux.tar.gz" "https://caddyserver.com/download/linux/386?license=personal" && caddy_bit="caddy_linux_386"
 	elif [[ ${bit} == "x86_64" ]]; then
-		wget -O "caddy_linux.tar.gz" "https://caddyserver.com/download/linux/amd64?license=personal" && caddy_bit="caddy_linux_amd64"
+		wget --no-check-certificate -O "caddy_linux.tar.gz" "https://caddyserver.com/download/linux/amd64?license=personal" && caddy_bit="caddy_linux_amd64"
 	else
 		echo -e "${Red}[错误]${Font} 不支持 ${bit} !" && exit 1
 	fi
@@ -57,25 +57,26 @@ Install_caddy(){
 	rm -rf CHANGES.txt
 	rm -rf "init/"
 	chmod +x caddy
-	wget https://raw.githubusercontent.com/FuaerCN/sh/master/Caddyfile
+	wget --no-check-certificate https://raw.githubusercontent.com/FuaerCN/sh/master/Caddyfile
 	
 }
 
 Install_aria2(){
-    wget https://raw.githubusercontent.com/FuaerCN/sh/master/Caddyfile && mv /root/.aria2
-	wget https://raw.githubusercontent.com/FuaerCN/sh/master/Caddyfile && mv /root/.aria2
-	wget https://raw.githubusercontent.com/FuaerCN/sh/master/Caddyfile && mv /root/.aria2
-	wget https://raw.githubusercontent.com/FuaerCN/sh/master/Caddyfile && mv /root/.aria2
+	cd /root/.aria2
+    wget --no-check-certificate -N https://raw.githubusercontent.com/FuaerCN/sh/master/aria2/aria2.conf
+    wget --no-check-certificate https://raw.githubusercontent.com/FuaerCN/sh/master/aria2/UpGDrive.sh && chmod +x UpGDrive.sh
+    wget --no-check-certificate https://raw.githubusercontent.com/FuaerCN/sh/master/aria2/UpOneDrive.sh && chmod +x UpOneDrive.sh.sh
+	cp -f /root/.aria2/UpGDrive.sh /root/.aria2/UpDrive.sh
 	
 }
 
 Install_aria2ng(){
     mkdir -p /home/AriaNg && cd /home/AriaNg
-	wget ${aria2ng_download_http} && unzip aria-ng-${aria2ng_new_ver}.zip
+	wget --no-check-certificate ${aria2ng_download_http} && unzip aria-ng-${aria2ng_new_ver}.zip
 }
 
 Install_oneindex(){
-    cd /home/ && wget -O oneindex.zip https://codeload.github.com/donwa/oneindex/zip/master && unzip oneindex.zip
+    cd /home/ && wget --no-check-certificate -O oneindex.zip https://codeload.github.com/donwa/oneindex/zip/master && unzip oneindex.zip
 	mv /home/oneindex-master /home/oneindex
 	chown www-data:www-data -R /home/oneindex/*
 }

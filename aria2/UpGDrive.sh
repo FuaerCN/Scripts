7a9990e5894e4c1f1f433e084b3477b5054a570d
Yeasty <fuaercn@gmail.com>
2018-08-09 03:06:33 +0800

    no message

---
+++ aria2/UpGDrive.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+path=$3
+downloadpath='/home/AriaNg/Download'
+if [ $2 -eq 0 ]
+        then
+                exit 0
+fi
+while true; do  #提取下载文件根路径，如把/root/downloads/a/b/c/d.jpg变成/root/downloads/a
+
+#删掉些BT中没用的辣鸡文件
+find "$path" -regextype posix-extended -regex ".*\.(chm|torrent|htm|html|url|nfo|ico|mht|gif)" -exec rm -f {} \;
+
+filepath=$path
+filename=${filepath%.*}
+extension=${filepath##*.}
+if [[ $extension = "zip" ]] ; then 
+	echo "解压 ZIP"
+	unzip -o -d $filename $filepath
+	#/usr/bin/php /home/oneindex/one.php upload:folder "$filename"/ /Aria2/"${filename##*/}"/
+	skicka upload "$filename"/ /Aria2/"${filename##*/}"/
+	#rm -rf "$filepath" "$filename"
+	rm -rf "$filename"
+	echo '解压文件上传'$path >> /root/UpDrive.log
+	
+elif [[ $extension = "rar11" ]] ; then 
+	echo "解压 RAR"
+	unzip -o -d $filename $filepath
+	#skicka upload "$filename"/ /Aria2/"${filename##*/}"/
+	rm -rf "$filepath" "$filename"
+	exit
+	
+else
+	echo "不解压"
+fi
+
+path=${path%/*};
+if [ "$path" = "$downloadpath" ] && [ $2 -eq 1 ]  #如果下载的是单个文件
+    then
+	#/usr/bin/php /home/oneindex/one.php upload:file "$filepath" /Aria2/
+	skicka upload "$filepath" /Aria2
+    rm -rf "$filepath"
+	echo '删除上传完成'$filepath >> /root/UpDrive.log
+    exit 0
+elif [ "$path" = "$downloadpath" ]   #文件夹
+    then
+	#/usr/bin/php /home/oneindex/one.php upload:folder "$filepath"/ /Aria2/"${filepath##*/}"/
+	skicka upload "$filepath"/ /Aria2/"${filepath##*/}"/
+    rm -rf "$filepath"/
+	echo '删除上传完成'$filepath >> /root/UpDrive.log
+    exit 0
+fi
+done
---
+++ test.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+apt-get install -y ca-certificates
+caddyfile="/usr/local/caddy/"
+caddy_file="/usr/local/caddy/caddy"
+caddy_conf_file="/usr/local/caddy/Caddyfile"
+aria2ng_new_ver=$(wget -qO- https://api.github.com/repos/mayswind/AriaNg/releases/latest | grep -o '"tag_name": ".*"' | sed 's/"//g' | sed 's/tag_name: //g') && echo ${Ver}
+aria2ng_download_http="https://github.com/mayswind/AriaNg/releases/download/${aria2ng_new_ver}/aria-ng-${aria2ng_new_ver}.zip"
+aria2_new_ver=$(wget -qO- "https://github.com/q3aql/aria2-static-builds/tags"| grep "/q3aql/aria2-static-builds/releases/tag/"| head -n 1| awk -F "/tag/v" '{print $2}'| sed 's/\">//') && echo -e "${aria2_new_ver}"
+
+Install_php(){
+	#Debian 8系统
+	#添加源
+	echo "deb http://packages.dotdeb.org jessie all" | tee --append /etc/apt/sources.list
+	echo "deb-src http://packages.dotdeb.org jessie all" | tee --append /etc/apt/sources.list
+	#添加key
+	wget https://www.dotdeb.org/dotdeb.gpg
+	apt-key add dotdeb.gpg
+	#更新系统
+	apt-get update -y
+	#安装PHP 7和Sqlite 3
+	apt-get install php7.0-cgi php7.0-fpm php7.0-curl php7.0-gd php7.0-mbstring php7.0-xml php7.0-sqlite3 sqlite3 -y
+}
+
+Install_caddy(){
+	bit=`uname -m`
+	if [[ -e ${caddy_file} ]]; then
+		echo && echo -e "${Red}[信息]${Font} 检测到 Caddy 已安装，是否继续安装(覆盖更新)？[y/N]"
+		stty erase '^H' && read -p "(默认: n):" yn
+		[[ -z ${yn} ]] && yn="n"
+		if [[ ${yn} == [Nn] ]]; then
+			echo && echo "已取消..." && exit 1
+		fi
+	fi
+	[[ ! -e ${caddyfile} ]] && mkdir "${caddyfile}"
+	cd "${caddyfile}"
+	PID=$(ps -ef |grep "caddy" |grep -v "grep" |grep -v "init.d" |grep -v "service" |grep -v "caddy_install" |awk '{print $2}')
+	[[ ! -z ${PID} ]] && kill -9 ${PID}
+	[[ -e "caddy_linux*.tar.gz" ]] && rm -rf "caddy_linux*.tar.gz"
+	
+	if [[ ${bit} == "i386" ]]; then
+		wget -O "caddy_linux.tar.gz" "https://caddyserver.com/download/linux/386?license=personal" && caddy_bit="caddy_linux_386"
+	elif [[ ${bit} == "i686" ]]; then
+		wget -O "caddy_linux.tar.gz" "https://caddyserver.com/download/linux/386?license=personal" && caddy_bit="caddy_linux_386"
+	elif [[ ${bit} == "x86_64" ]]; then
+		wget -O "caddy_linux.tar.gz" "https://caddyserver.com/download/linux/amd64?license=personal" && caddy_bit="caddy_linux_amd64"
+	else
+		echo -e "${Red}[错误]${Font} 不支持 ${bit} !" && exit 1
+	fi
+	[[ ! -e "caddy_linux.tar.gz" ]] && echo -e "${Red}[错误]${Font} Caddy 下载失败 !" && exit 1
+	tar zxf "caddy_linux.tar.gz"
+	rm -rf "caddy_linux.tar.gz"
+	[[ ! -e ${caddy_file} ]] && echo -e "${Red}[错误]${Font} Caddy 解压失败或压缩文件错误 !" && exit 1
+	rm -rf LICENSES.txt
+	rm -rf README.txt 
+	rm -rf CHANGES.txt
+	rm -rf "init/"
+	chmod +x caddy
+	wget https://raw.githubusercontent.com/FuaerCN/sh/master/Caddyfile
+	
+}
+
+Install_aria2(){
+    wget https://raw.githubusercontent.com/FuaerCN/sh/master/Caddyfile && mv /root/.aria2
+	wget https://raw.githubusercontent.com/FuaerCN/sh/master/Caddyfile && mv /root/.aria2
+	wget https://raw.githubusercontent.com/FuaerCN/sh/master/Caddyfile && mv /root/.aria2
+	wget https://raw.githubusercontent.com/FuaerCN/sh/master/Caddyfile && mv /root/.aria2
+	
+}
+
+Install_aria2ng(){
+    mkdir -p /home/AriaNg && cd /home/AriaNg
+	wget ${aria2ng_download_http} && unzip aria-ng-${aria2ng_new_ver}.zip
+}
+
+Install_oneindex(){
+    cd /home/ && wget -O oneindex.zip https://codeload.github.com/donwa/oneindex/zip/master && unzip oneindex.zip
+	mv /home/oneindex-master /home/oneindex
+	chown www-data:www-data -R /home/oneindex/*
+}
+
+main(){
+	Install_php
+	caddy_install
+	Install_aria2
+	Install_aria2ng
+	Install_oneindex
+}
+
+main

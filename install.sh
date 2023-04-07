4c758d4e9723ea9e5a77dc3fa3b56d892a6c30c9
FuaerCN <FuaerCN@Gmail.com>
2023-04-07 14:01:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get install wget curl -y
+
 sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
 echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
